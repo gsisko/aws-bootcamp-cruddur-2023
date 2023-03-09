3c1c763ddde5358a9998c5949e5238fecb3019db
Gabriel Isko <gabestripes@gmail.com>
2023-03-09 21:16:05 +0000

    make changes for cognito in front end

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 
 import HomeFeedPage from './pages/HomeFeedPage';
 import UserFeedPage from './pages/UserFeedPage';
-import NotificationsFeedPage from './pages/NotificationsFeedPage';
 import SignupPage from './pages/SignupPage';
 import SigninPage from './pages/SigninPage';
 import RecoverPage from './pages/RecoverPage';
@@ -15,6 +14,23 @@ import {
   createBrowserRouter,
   RouterProvider
 } from "react-router-dom";
+
+import { Amplify } from 'aws-amplify';
+
+Amplify.configure({
+  "AWS_PROJECT_REGION": process.env.REACT_APP_AWS_PROJECT_REGION,
+  "aws_cognito_region": process.env.REACT_APP_AWS_COGNITO_REGION,
+  "aws_user_pools_id": process.env.REACT_APP_AWS_USER_POOLS_ID,
+  "aws_user_pools_web_client_id": process.env.REACT_APP_CLIENT_ID,
+  "oauth": {},
+  Auth: {
+    // We are not using an Identity Pool
+    // identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID, // REQUIRED - Amazon Cognito Identity Pool ID
+    region: process.env.REACT_APP_AWS_PROJECT_REGION,           // REQUIRED - Amazon Cognito Region
+    userPoolId: process.env.REACT_APP_AWS_USER_POOLS_ID,         // OPTIONAL - Amazon Cognito User Pool ID
+    userPoolWebClientId: process.env.REACT_APP_CLIENT_ID,   // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+  }
+});
 
 const router = createBrowserRouter([
   {
@@ -32,10 +48,6 @@ const router = createBrowserRouter([
   {
     path: "/messages/@:handle",
     element: <MessageGroupPage />
-  },
-  {
-    path: "/notifications",
-    element: <NotificationsFeedPage />
   },
   {
     path: "/signup",
