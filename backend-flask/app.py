2dfeb8138d0b745f6ce2b5c71f0e8e95cdcbca43
Gabriel Isko <gabestripes@gmail.com>
2023-03-21 17:42:10 +0000

    fix CORS shenanigans

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -90,12 +90,12 @@ cors = CORS(
 #      #TODO add logging here.
 
 
-@app.after_request
-def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', '*')
-  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  return response
+#@app.after_request
+#def after_request(response):
+#  response.headers.add('Access-Control-Allow-Origin', '*')
+#  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+#  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+#  return response
 
 
 
