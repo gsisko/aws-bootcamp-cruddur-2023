841a21c8609f6a097d71a1e1be18fdd20c1cc097
Gabriel Isko <gabestripes@gmail.com>
2023-03-21 02:13:48 +0000

    fix code to get database connection working

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -4,7 +4,7 @@ from db import pool, query_wrap_array
 
 class HomeActivities:
   def run():
-    sql = query_wrap_array("""
+    sql = query_wrap_array('''
       SELECT
         activities.uuid,
         users.display_name,
@@ -19,12 +19,12 @@ class HomeActivities:
       FROM public.activities
       LEFT JOIN public.users ON users.uuid = activities.user_uuid
       ORDER BY activities.created_at DESC
-      """)
-      print(sql)
-      with pool.connection() as conn:
-        with conn.cursor() as cur:
+      ''')
+    print(sql)
+    with pool.connection() as conn:
+      with conn.cursor() as cur:
           cur.execute(sql)
           # this will return a tuple
           # the first field being the data
           json = cur.fetchone()
-      return json[0]
+    return json[0]
