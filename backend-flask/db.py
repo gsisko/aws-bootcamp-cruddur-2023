b2fa9f3b29e6f9d68b7095ccc32c056b5dd0981a
Gabriel Isko <gabestripes@gmail.com>
2023-03-21 06:05:35 +0000

    add whitespace

--- backend-flask/db.py
+++ backend-flask/db.py
@@ -8,5 +8,6 @@ def query_wrap_object(template):
 def query_wrap_array(template):
   sql = f"""(SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM ({template}) array_row);"""
   return sql
+
 connection_url = os.getenv("DB_CONNECTION_URL")
 pool = ConnectionPool(connection_url)
